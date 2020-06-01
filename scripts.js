52d938247d2e9ae61b07087c9ba0eeb69d8d8ee4
Daniel Dolan <danieldolan7@gmail.com>
2020-06-01 14:25:12 -0400

    buttons work, styling not complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("Scripts file linked!");
+$(document).ready(function() {
+
+	function printHTML(text){
+		var printHTML = document.getElementById('text');
+		printHTML.innerHTML += text;
+	}
+
+
+	$('#buttonOne').click(function(){
+        $('.text2').remove();
+			printHTML("<h2 class='text1'>Told ya I was right! </h2>" );
+		})
+
+	$('#buttonTwo').click(function(){
+        $('.text1').remove();
+			printHTML("<h2 class='text2'> Loser, Haha I was right!! </h2>");
+    })
+})
